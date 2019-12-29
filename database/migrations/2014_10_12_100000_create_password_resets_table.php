f8dfd11bebc231587eeb4985edb16ececb819bd5
Felix Morau <34188098+femosc2@users.noreply.github.com>
2019-12-29 23:00:22 +0100

    Revert "Resturants"

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateResturantsTable extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateResturantsTable extends Migration
      */
     public function up()
     {
-        Schema::create('resturants', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('location');
-            $table->timestamps();
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -28,6 +27,6 @@ class CreateResturantsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('resturants');
+        Schema::dropIfExists('password_resets');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,18 @@
 <?php
 
-use App\Resturant;
 use Illuminate\Http\Request;
 
-Route::get('/resturants', 'Resturants@index');
-Route::post('/resturants/new', 'Resturants@create');
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-
-
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
