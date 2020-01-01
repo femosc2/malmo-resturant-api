63ee2c7981e8449230dc062e626e7eddb25e24de
Felix Morau <felixmorau@gmail.com>
2020-01-01 20:06:41 +0100

    fix merge conflict

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:database/migrations/2019_12_29_001552_create_resturants_table.php
         Schema::create('resturants', function (Blueprint $table) {
             $table->bigIncrements('id')->onDelete('cascade');
-=======
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
->>>>>>> 44e7a77dbfacb46eddd275252e5bb48783675007:database/migrations/2014_10_12_000000_create_users_table.php
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
